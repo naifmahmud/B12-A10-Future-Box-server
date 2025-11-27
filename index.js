e09d2c0e58bc15da4c2fc978b0fd576a27f40305
naif <naifmahmud777@gmail.com>
2025-11-27 15:31:24 +0600

    Connect the server with mongodb database

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express= require("express");
 const cors= require("cors");
 const port= process.env.PORT || 3000;
@@ -6,10 +7,41 @@ const port= process.env.PORT || 3000;
 
 const app=express();
 
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.db_username}:${process.env.db_password}@cluster0.4guptnm.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
 app.get('/',(req,res)=>{
     res.send("My server is running")
 })
+
+
+async function run(){
+    try{
+        await client.connect();
+
+        await client.db("admin").command({ping:1});
+            console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
+
+
+
 
 app.listen(port,()=>{
     console.log(`my server is running on PORT: ${port}`);
